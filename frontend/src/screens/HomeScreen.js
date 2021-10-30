07918e8bf6e76719d73af9a85c0f28f6c89d4be4
Cvijetin Mladjenovic <mladjenovic.cvijetin@gmail.com>
2021-10-30 15:19:08 -0700

    Project skeleton
    Added frontend an backend folders for project baseline

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>List of tests</h1>
+      <Row></Row>
+    </div>
+  );
+}
+
+export default HomeScreen;
