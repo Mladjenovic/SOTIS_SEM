08ef0b9902cde36a66f84f7a7da1f9b501925677
Cvijetin Mladjenovic <mladjenovic.cvijetin@gmail.com>
2021-11-27 20:25:39 +0100

    Implemented tests contoler

---
+++ backend_dotnet/backend/Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using backend.Models;
+using backend.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ITestRepository _repository;
+        public TestController(ITestRepository repository)
+        {
+            _repository = repository;
+        }
+        //GET: api/Test
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Test>>> GetTests()
+        {
+            return await _repository.GetTests();
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Test>> GetTest(int id)
+        {
+            return await _repository.GetTest(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Test>> PostTest(Test test)
+        {
+            return await _repository.PostTest(test);
+        }
+
+        [HttpPut ("{id}")]
+        public async Task<ActionResult<Test>> PostTest(int id, Test test)
+        {
+            return await _repository.PutTest(id,test);
+        }
+
+        [HttpDelete ("{id}")]
+        public async Task DeleteTest(int id)
+        {
+            await _repository.DeleteTest(id);
+        }
+
+
+    }
+}
